87d8395a93c2918548224b2a487bd6bb2d619753
Ayxan <ayxan.axmed26@gmail.com>
2022-08-15 01:22:09 +0400

    HTML-CSS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const navMenu=document.querySelector(".nav-menu")
+const menu =document.querySelector("#header .header-middle")
+
+navMenu.addEventListener("click",function(){
+    menu.classList.toggle("menu-active")
+})
+
+const header=document.getElementById("header")
+window.onscroll=function(){
+    if(this.scrollY>100){
+        header.classList.add("header-active")
+    }
+    else{
+        header.classList.remove("header-active")
+    }
+}
+
+
